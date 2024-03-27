ee2c86ff70a126eaa6929602fd6b53157bb75264
StarPandaBeg <kshmurakov@mail.ru>
2024-03-27 21:56:55 +0300

    Add page header Setup page layout

---
+++ components/HeaderMain.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+import {
+  MagnifyingGlassIcon,
+  HeartIcon,
+  ShoppingBagIcon,
+} from "@heroicons/vue/24/outline";
+</script>
+
+<template>
+  <header class="flex justify-between items-center gap-6">
+    <div>
+      <VLink to="/" variant="plain" title="Mood">
+        <img src="~/assets/images/mood.png" class="size-8" />
+      </VLink>
+    </div>
+    <div class="grow flex items-center">
+      <nav>
+        <ul class="flex font-medium">
+          <li>
+            <VLink variant="background" class="px-3 py-4">Новинки</VLink>
+          </li>
+          <li>
+            <VLink variant="background" class="px-3 py-4">Бренды</VLink>
+          </li>
+          <li>
+            <VLink variant="background" class="px-3 py-4">Мужское</VLink>
+          </li>
+          <li>
+            <VLink variant="background" class="px-3 py-4">Женское</VLink>
+          </li>
+          <li>
+            <VLink variant="background" class="px-3 py-4">Аксессуары</VLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div>
+      <ul class="flex gap-6">
+        <li>
+          <VLink>
+            <MagnifyingGlassIcon class="size-6" />
+          </VLink>
+        </li>
+        <li>
+          <VLink>
+            <HeartIcon class="size-6" />
+          </VLink>
+        </li>
+        <li>
+          <VLink>
+            <ShoppingBagIcon class="size-6" />
+          </VLink>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,9 @@
 <template>
-  <div id="main">
-    <NuxtPage />
+  <div id="main" class="container min-h-screen bg-slate-50">
+    <div class="px-5 py-2.5">
+      <HeaderAdditional />
+      <HeaderMain class="pt-6" />
+      <NuxtPage />
+    </div>
   </div>
 </template>
