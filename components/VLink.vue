20889ffe1f539922e3f6b5bebb64f3eb334be5ad
StarPandaBeg <kshmurakov@mail.ru>
2024-03-27 21:39:36 +0300

    Update VLink - add multiple variants

--- components/VLink.vue
+++ components/VLink.vue
@@ -1,12 +1,21 @@
 <script lang="ts" setup>
 import type { VLinkProps } from "~/types/vlink.types";
 
+const classes = {
+  default: "opacity-50 hover:opacity-100 active:opacity-75",
+  plain: "",
+  background: "hover:bg-black/5 active:bg-black/10",
+};
+
 const props = withDefaults(defineProps<VLinkProps>(), {
   to: "#",
   external: false,
   newtab: false,
+  variant: "default",
 });
+
 const target = computed(() => (props.newtab ? "_blank" : ""));
+const variantClass = computed(() => classes[props.variant]);
 </script>
 
 <template>
@@ -14,11 +23,11 @@ const target = computed(() => (props.newtab ? "_blank" : ""));
     v-if="external || newtab"
     :href="props.to"
     :target="target"
-    class="opacity-50 hover:opacity-100"
+    :class="variantClass"
   >
     <slot />
   </a>
-  <NuxtLink v-else :to="props.to" class="opacity-50 hover:opacity-100">
+  <NuxtLink v-else :to="props.to" :class="variantClass">
     <slot />
   </NuxtLink>
 </template>
--- types/vlink.types.ts
+++ types/vlink.types.ts
@@ -2,4 +2,5 @@ export type VLinkProps = {
   to?: string;
   external?: boolean;
   newtab?: boolean;
+  variant?: "default" | "plain" | "background";
 };
