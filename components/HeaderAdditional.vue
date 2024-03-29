2b657f61b541dd5abae8b04d00f6b5c572107bf3
StarPandaBeg <kshmurakov@mail.ru>
2024-03-29 17:31:07 +0300

    Fix: Make HeaderAdditional adaptive

--- components/HeaderAdditional.vue
+++ components/HeaderAdditional.vue
@@ -1,9 +1,9 @@
 <template>
-  <nav class="flex justify-between text-sm">
-    <div>
+  <nav class="flex justify-between text-sm text-center sm:text-left">
+    <div class="grow">
       <VLink>О проекте</VLink>
     </div>
-    <div class="flex gap-4">
+    <div class="hidden sm:flex gap-4">
       <VLink>Покупателю</VLink>
       <VLink>Поставщику</VLink>
       <VLink>О бренде</VLink>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="main" class="container flex flex-col min-h-screen bg-slate-50">
+  <div id="main" class="flex flex-col min-h-screen bg-slate-50">
     <div class="px-5 py-2.5">
       <HeaderAdditional />
-      <HeaderMain class="pt-6" />
+      <HeaderMain class="pt-4 md:pt-6" />
       <NuxtPage />
     </div>
     <FooterMain class="mt-auto" />
