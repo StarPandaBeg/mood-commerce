ca19084cdbe4a2624c11553b65374ce79450bad3
StarPandaBeg <kshmurakov@mail.ru>
2024-03-29 15:33:07 +0300

    Feature: Update index page template
    Add carousel for advertisment banners

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Hello World!</h1>
+  <VCarousel class="w-100">
+    <VCarouselItem
+      class="flex justify-between bg-no-repeat bg-cover rounded"
+      id="banner-1"
+    >
+      <div class="px-16 flex flex-col justify-center">
+        <h1 class="font-bold text-6xl">SUMMER ‘24</h1>
+        <p class="text-2xl">Летняя коллекция футболок только для тебя</p>
+      </div>
+      <img src="~/assets/images/t-shirts.png" alt="" width="512" />
+    </VCarouselItem>
+
+    <template #modules>
+      <VCarouselNavigation direction="left" />
+    </template>
+  </VCarousel>
 </template>
+
+<style lang="scss" scoped>
+#banner-1 {
+  background-image: url(~/assets/images/banner-1-pattern.png);
+}
+</style>
