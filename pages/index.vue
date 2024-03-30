2a5f2bbfceda8b1f8110a729abe787309a9abd12
StarPandaBeg <kshmurakov@mail.ru>
2024-03-30 15:22:10 +0300

    Feature: Update banner on index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,7 @@
 <template>
   <VCarousel class="w-100">
-    <VCarouselItem
-      class="flex justify-between bg-no-repeat bg-cover rounded"
-      id="banner-1"
-    >
-      <div class="px-16 flex flex-col justify-center">
-        <h1 class="font-bold text-6xl">SUMMER ‘24</h1>
-        <p class="text-2xl">Летняя коллекция футболок только для тебя</p>
-      </div>
-      <img src="~/assets/images/t-shirts.png" alt="" width="512" />
+    <VCarouselItem class="rounded">
+      <img src="~/assets/images/banner-1.png" class="w-full" />
     </VCarouselItem>
 
     <template #modules>
@@ -16,9 +9,3 @@
     </template>
   </VCarousel>
 </template>
-
-<style lang="scss" scoped>
-#banner-1 {
-  background-image: url(~/assets/images/banner-1-pattern.png);
-}
-</style>
