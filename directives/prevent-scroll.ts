26a6ed7e406c36077fad1c946f5c796d35675bb2
StarPandaBeg <kshmurakov@mail.ru>
2024-03-30 14:37:17 +0300

    Fix: Make v-prevent-scroll directive to use custom class instead of tailwind ones
    Tailwind classes sometimes are not resolved automatically, so it would better to use own class

--- directives/prevent-scroll.ts
+++ directives/prevent-scroll.ts
@@ -3,10 +3,10 @@ import type { Directive } from "vue";
 export default <Directive>{
   mounted: (el, binding) => {
     const preventScroll = (binding.value as boolean | undefined) ?? true;
-    document.body.classList.toggle("overflow-hidden", preventScroll);
+    document.body.classList.toggle("prevent-scroll", preventScroll);
   },
   updated: (el, binding) => {
     const preventScroll = (binding.value as boolean | undefined) ?? true;
-    document.body.classList.toggle("overflow-hidden", preventScroll);
+    document.body.classList.toggle("prevent-scroll", preventScroll);
   },
 };
